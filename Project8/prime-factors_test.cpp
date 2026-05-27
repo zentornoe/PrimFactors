a1f199d2c5d46c8089738925b1b58bc5770a5b3a
zentornoe <andycityfc@gmail.com>
2026-05-28 08:59:43 +0900

    [feature] 27

--- Project8/prime-factors_test.cpp
+++ Project8/prime-factors_test.cpp
@@ -6,3 +6,9 @@ TEST(PrimeFactors, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
