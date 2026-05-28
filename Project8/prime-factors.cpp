51fd92197ba0df1ed3a7d86d5119b34bb3ac64c4
zentornoe <andycityfc@gmail.com>
2026-05-28 09:04:44 +0900

    [feature] 30

--- Project8/prime-factors.cpp
+++ Project8/prime-factors.cpp
@@ -6,7 +6,11 @@ class PrimeFactors {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
+
 		if (number == 2) result.push_back(2);
+
+		if (number == 3) result.push_back(3);
+
 		return result;
 	}
 };
