978d28cc5d4dee4186a2ab0df2a402ac1ce18eb9
zentornoe <andycityfc@gmail.com>
2026-05-28 09:09:45 +0900

    [feature] 38

--- Project8/prime-factors_test.cpp
+++ Project8/prime-factors_test.cpp
@@ -27,3 +27,8 @@ TEST_F(PrimeFixture, Of43) {
 	expected = { 2, 2 };
 	EXPECT_EQ(expected, prime_factor.of(4));
 }
+
+TEST_F(PrimeFixture, Of6) {
+	expected = { 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
