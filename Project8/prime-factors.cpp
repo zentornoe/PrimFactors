a9d4c0d851aa9219192430e7d4f4dbe6cbf93bb7
zentornoe <andycityfc@gmail.com>
2026-05-28 09:33:01 +0900

    [refactor] 49

--- Project8/prime-factors.cpp
+++ Project8/prime-factors.cpp
@@ -6,19 +6,11 @@ class PrimeFactors {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1) {
-			int divisor = 2;
-			if (number == 4 || number == 6 || number == 9 || number == 12) {
-				for (divisor = 2; number > 1; divisor++) {
-					while (number % divisor == 0) {
-						result.push_back(divisor);
-						number /= divisor;
-					}
-				}
-				divisor++;
-			}
-			else {
-				result.push_back(number);
+		int divisor = 2;
+		for (divisor = 2; number > 1; divisor++) {
+			while (number % divisor == 0) {
+				result.push_back(divisor);
+				number /= divisor;
 			}
 		}
 		return result;
