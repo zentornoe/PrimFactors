5b2a7efda148b9ef55aa0539d4023086d7685640
zentornoe <andycityfc@gmail.com>
2026-05-28 09:01:30 +0900

    [refactor] 28

--- Project8/prime-factors_test.cpp
+++ Project8/prime-factors_test.cpp
@@ -1,14 +1,19 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-TEST(PrimeFactors, Of1) {
+using namespace testing;
+class PrimeFixture : public Test {
+public:
 	PrimeFactors prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	expected = { };
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFactors, Of2) {
-	PrimeFactors prime_factor;
-	vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = { 2 };
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
