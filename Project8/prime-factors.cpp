6461ff4b8073b3886f415e1a4d81d761f2723618
zentornoe <andycityfc@gmail.com>
2026-05-28 09:13:46 +0900

    [feature] 44

--- Project8/prime-factors.cpp
+++ Project8/prime-factors.cpp
@@ -23,6 +23,10 @@ public:
 				}
 				divisor++;
 			}
+			else if (number == 9) {
+				result.push_back(3);
+				result.push_back(3);
+			}
 			else {
 				result.push_back(number);
 			}
--- Project8/prime-factors_test.cpp
+++ Project8/prime-factors_test.cpp
@@ -32,3 +32,8 @@ TEST_F(PrimeFixture, Of6) {
 	expected = { 2, 3 };
 	EXPECT_EQ(expected, prime_factor.of(6));
 }
+
+TEST_F(PrimeFixture, Of9) {
+	expected = { 3, 3 };
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
